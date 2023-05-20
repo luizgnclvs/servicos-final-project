72fc947241c82793c73356381fc40f0935519cb4
luizgnclvs <luizgnclvss@gmail.com>
2023-05-20 17:08:15 -0300

    Add métofdos default

--- src/index.js
+++ src/index.js
@@ -11,6 +11,22 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+	return res.send("Received a GET HTTP method");
+});
+
+app.post("/", (req, res) => {
+	return res.send("Received a POST HTTP method");
+});
+
+app.put("/", (req, res) => {
+	return res.send("Received a PUT HTTP method");
+});
+
+app.delete("/", (req, res) => {
+	return res.send("Received a DELETE HTTP method");
+});
+
 const PORT = process.env.PORT || 3000;
 const eraseDatabaseOnSync = process.env.ERASE_DATABASE_ON_SYNC === "true";
 
