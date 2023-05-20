f9a6e650c14ed76089e1f1f69e69d8854e3fa3b2
luizgnclvs <luizgnclvss@gmail.com>
2023-05-20 17:29:53 -0300

    Add método POST e GET by Id

--- src/routes/album.js
+++ src/routes/album.js
@@ -12,3 +12,31 @@ router.get('/', async (req, res) => {
 		res.status(500).json({ error: 'Erro ao obter os álbuns' });
 	}
 });
+
+router.get('/:albumId', async (req, res) => {
+	try {
+		const { albumId } = req.params;
+
+		const album = await Album.findByPk(albumId);
+
+		if (!album) {
+			return res.status(404).json({ error: 'Álbum não encontrado' });
+		}
+
+		res.status(200).json(album);
+	} catch (error) {
+		res.status(500).json({ error: 'Erro ao obter o álbum' });
+	}
+});
+
+router.post('/', async (req, res) => {
+	try {
+		const { name, artist, cover_url } = req.body;
+
+		const album = await Album.create({ name, artist, cover_url });
+
+		res.status(201).json(album);
+	} catch (error) {
+		res.status(500).json({ error: 'Erro ao criar o álbum' });
+	}
+});
